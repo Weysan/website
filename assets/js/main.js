67892ab608f756a94bc0c4dd6533c7e4b508ff00
raphael <raphael@spectrm.de>
2019-12-25 10:57:11 +0100

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$('.page-size-75').css('height', window.outerHeight * 0.75);
+
+// Troll people who try to check logs on my website
+if( typeof console === 'object' ) {
+    console.log(
+        '\n' +
+        'Hello there!\n' +
+        'If you are here, I guess it\'s because you don\'t like my design skills?\n' +
+        'I actually can\'t blame you for that. So all good, I did a small resume in the console too.\n' +
+        '\n' +
+        'My name is Raphaël Gonçalves, and I\'m currently working for Spectm.io, and I also lead the Facebook Developer Circle Berlin. \n' +
+        'It is a super cool community, you should join sometimes if you are around. \n' +
+        'You can follow my twitter where I post some random stuff: https://twitter.com/nihon_rafy \n' +
+        '\n' +
+        'PS: please don\'t break my website.' +
+        'xoxo'
+    );
+}
